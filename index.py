5e7b4d830ece88e174020322c901cce4185a0646
Daniel <carlosdanielguth@gmail.com>
2024-05-11 17:41:33 -0300

    Porcentage machine

---
+++ index.py
@@ -0,0 +1,5 @@
+num = int(input("Valor:"))
+value_porcentage = float(input ("Digite o valor que desejar:"))
+count = num/value_porcentage*100
+print("{}%".format(count))
+input("Digite novamente para sair")
